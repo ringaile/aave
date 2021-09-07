0a5b698e01c1af176baf749c18ecb290876e3d4d
Ringaile Valiaugaite <ringaile@gmail.com>
2021-09-07 23:18:51 +0200

    added uniswap router

--- contracts/Aave.sol
+++ contracts/Aave.sol
@@ -4,16 +4,21 @@ pragma solidity ^0.6.12;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@aave/protocol-v2/contracts/interfaces/ILendingPoolAddressesProvider.sol";
 import "@aave/protocol-v2/contracts/interfaces/ILendingPool.sol";
+import './interfaces/IUniswapV2Router02.sol';
 
 contract Aave{
 
     ILendingPoolAddressesProvider lendingPoolAddressesProvider;
     ILendingPool lendingPool;
+    IUniswapV2Router02 uniswapV2Router02;
 
     constructor() public{
         // check the latest address from here: https://docs.aave.com/developers/deployed-contracts/deployed-contracts                                                         
         lendingPoolAddressesProvider = ILendingPoolAddressesProvider(0x88757f2f99175387aB4C6a4b3067c77A695b0349);
         lendingPool = ILendingPool(lendingPoolAddressesProvider.getLendingPool());
+
+        // the address found here: https://docs.uniswap.org/protocol/V2/reference/smart-contracts/router-02
+        uniswapV2Router02 = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
     }
 
     function deposit(address _token, uint _amount) external {
@@ -24,5 +29,18 @@ contract Aave{
 
         token.approve(address(lendingPool), _amount);
         lendingPool.deposit(_token, _amount, address(this), referral);
+    }
+
+    function withdraw(address _token, uint _amount) external payable{
+        lendingPool.withdraw(_token, _amount, address(this));
+
+        uniswapV2Router02.addLiquidityETH{value: msg.value}(
+                _token,
+                _amount,
+                1,
+                1,
+                address(this),
+                block.timestamp + 1000
+            );
     }
 }
