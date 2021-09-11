918a917f3e90b079a4e10026af358689323ce1cd
Ringaile Valiaugaite <ringaile@gmail.com>
2021-09-11 21:30:09 +0200

    added kovan

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,9 +13,25 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "";
+
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const KOVAN_PRIVATE_KEY = "";
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.6.12",
+  networks: {
+    kovan: {
+      url: `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${KOVAN_PRIVATE_KEY}`],
+    },
+  },
 };
